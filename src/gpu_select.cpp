31a22a304d07d21db4e8922afb57a3c76ba2fca8
SizuYuri <gjk2431@gmail.com>
2025-11-25 22:28:48 +0300

    Delete gpu_select.cpp

--- src/gpu_select.cpp
+++
@@ -1,9 +0,0 @@
-// Просим дискретную GPU на ноутбуках с Optimus/PowerXpress
-#ifdef _WIN32
-extern "C" {
-	// NVIDIA Optimus
-	__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
-	// AMD PowerXpress
-	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-}
-#endif
